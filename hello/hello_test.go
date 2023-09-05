3ff76b104aa3f929951a5524709d916a302d0f03
mach4101 <mach4101@gmail.com>
2023-09-05 18:55:50 +0800

    add assert

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("mach")
-	want := "Hello, mach"
-
-	if got != want {
-		t.Errorf("got '%q' want '%q'", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("mach")
+		want := "Hello, mach"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
