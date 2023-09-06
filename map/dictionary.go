d4cecbfef97471bf1a0003eb625234329e20598f
mach4101 <mach4101@gmail.com>
2023-09-06 15:05:19 +0800

    Test Search and Test Add is all ok, except for the repeat add op

---
+++ map/dictionary.go
@@ -0,0 +1,20 @@
+package main
+
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
+}
