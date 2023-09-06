01ce58535df1c28d77d5aa6db8b73a2182015f71
mach4101 <mach4101@gmail.com>
2023-09-06 15:23:20 +0800

    Test Search and Test Add is all ok

--- map/dictionary_test.go
+++ map/dictionary_test.go
@@ -22,13 +22,26 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
 
-	word := "test"
-	definition := "this is just a test"
+		word := "test"
+		definition := "this is just a test"
 
-	dictionary.Add(word, definition)
+		dictionary.Add(word, definition)
+		assertDefinition(t, dictionary, word, definition)
+	})
 
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, "new test")
+
+		assertErrors(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
