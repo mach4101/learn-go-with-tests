aac3b22081610f760c60ed74b97dc1640dc25c56
mach4101 <mach4101@gmail.com>
2023-09-06 16:06:34 +0800

    last version of map

--- map/dictionary_test.go
+++ map/dictionary_test.go
@@ -68,6 +68,18 @@ func TestUpdate(t *testing.T) {
 	})
 }
 
+func TestDelete(t *testing.T) {
+	word := "test"
+
+	dictionary := Dictionary{word: "test definition"}
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("Expected '%s' to be deleted!", word)
+	}
+}
+
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 
