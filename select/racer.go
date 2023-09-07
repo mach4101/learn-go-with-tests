f3b0f1fe4e90688aaa408e2a204afb7721bfb107
mach4101 <mach4101@gmail.com>
2023-09-07 12:01:42 +0800

    last version of select

--- select/racer.go
+++ select/racer.go
@@ -1,23 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
 func Racer(a, b string) (winner string) {
-	aTime := measureResponseTime(a)
-	bTime := measureResponseTime(b)
-
-	if aTime < bTime {
+	select {
+	case <-ping(a):
 		return a
-	} else {
+	case <-ping(b):
 		return b
 	}
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
 }
--- select/racer_test.go
+++ select/racer_test.go
@@ -8,24 +8,38 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(20 * time.Millisecond)
+	t.Run("compares speed of servers, returning the url of the fast one", func(t *testing.T) {
+		slowServer := makeDelayedServer(10 * time.Millisecond)
+		defer slowServer.Close()
+
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got := Racer(slowURL, fastURL)
+
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
 		w.WriteHeader(http.StatusOK)
 	}))
-	defer slowServer.Close()
-
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
-	defer fastServer.Close()
-
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
-
-	want := fastURL
-	got := Racer(slowURL, fastURL)
-
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
-	}
 }
