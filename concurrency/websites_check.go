ab99484d1422b4b0ac829bbf7fd22208e744c51f
mach4101 <mach4101@gmail.com>
2023-09-07 11:13:14 +0800

    channel version of websites checker

--- concurrency/websites_check.go
+++ concurrency/websites_check.go
@@ -1,21 +1,28 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
-
+	resultChannel := make(chan result)
 	// wg := sync.WaitGroup{}
 	// wg.Add(len(urls))
 	for _, url := range urls {
 		go func(url string) {
-			results[url] = wc(url)
+			resultChannel <- result{url, wc(url)}
 			// wg.Done()
 		}(url)
 	}
 	// wg.Wait()
-	time.Sleep(2 * time.Second)
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
+
 	return results
 }
