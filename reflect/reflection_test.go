54c575201f857484b1fc8c214e5db5562a465d53
mach4101 <mach4101@gmail.com>
2023-09-08 10:09:13 +0800

    complex struct with reflection

--- reflect/reflection_test.go
+++ reflect/reflection_test.go
@@ -1,24 +1,46 @@
 package reflection
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
-	expected := "mach"
-	var got []string
-
-	x := struct {
-		Name string
-	}{expected}
-
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"mach"},
+			[]string{"mach"},
+		}, {
+			"Struct with two string field",
+			struct {
+				Name string
+				City string
+			}{
+				"mach",
+				"China",
+			},
+			[]string{"mach", "China"},
+		},
 	}
 
-	if got[0] != expected {
-		t.Errorf("got '%s' want '%s'", got[0], expected)
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
+
 }
